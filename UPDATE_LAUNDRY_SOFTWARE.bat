797571d68e39852a769bba4ac2322559459fb182
prince.jio.tigley <prince.jio.tigley@accenture.com>
2023-07-25 13:47:21 +0800

    stockupdater

--- UPDATE_LAUNDRY_SOFTWARE.bat
+++ UPDATE_LAUNDRY_SOFTWARE.bat
@@ -1,1 +1,10 @@
+@echo off
+
+REM Set the path to your htdocs folder
+set "htdocs_path=C:\xampp\htdocs\stock\"
+
+REM Change to the htdocs directory
+cd /d "%htdocs_path%"
+
+REM Execute git pull
 git pull
